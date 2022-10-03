81475fc8c0f6260afda3f64481865ccb6df21a9c
余礼钵 <564885081@qq.com>
2022-10-03 16:06:46 +0800

    Initial commit

---
+++ Example/YLBDServices/YLBViewController.h
@@ -0,0 +1,13 @@
+//
+//  YLBViewController.h
+//  YLBDServices
+//
+//  Created by 余礼钵 on 10/03/2022.
+//  Copyright (c) 2022 余礼钵. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YLBViewController : UIViewController
+
+@end
---
+++ Example/YLBDServices/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  YLBDServices
+//
+//  Created by 余礼钵 on 10/03/2022.
+//  Copyright (c) 2022 余礼钵. All rights reserved.
+//
+
+@import UIKit;
+#import "YLBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YLBAppDelegate class]));
+    }
+}
